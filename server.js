6444151daee2df8dd1b5bddf8936bc0b370ccb26
ArnoldPires <arnold.pires92@gmail.com>
2021-04-03 17:21:46 -0400

    setup is done

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const MongoClient = require("mongodb").MongoClient;
+const PORT = 8000;
+require("dotenv").config();
+
+let db,
+  dbConnectionStr = process.env.DB_STRING,
+  dbName = "To-Do-List";
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
+  (client) => {
+    console.log(`Connected to ${dbName} Database`);
+    db = client.db(dbName);
+  }
+);
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  db.collection("to-do-list")
+    .find()
+    .toArray()
+    .then((data) => {
+      response.render("index.ejs", { info: data });
+    })
+    .catch((error) => console.error(error));
+});
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
